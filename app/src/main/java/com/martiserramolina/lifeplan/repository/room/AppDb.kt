7026aeef404eb0d75cea64820337db7e878e431a
martiserra46 <martiserra385@gmail.com>
2020-10-17 11:49:13 +0200

    Setup AppDb in repository

---
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/room/AppDb.kt
@@ -0,0 +1,48 @@
+package com.martiserramolina.lifeplan.repository.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [
+        YourLifeDb::class,
+        TopicDb::class,
+        IdeaDb::class,
+        SituationDayDb::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(
+    IdeaImportanceConverterDb::class,
+    SituationDaySatisfactionConverterDb::class,
+    DateConverterDb::class
+)
+abstract class AppDb : RoomDatabase() {
+    companion object {
+        private lateinit var INSTANCE: AppDb
+        private const val DATABASE_NAME = "app_db"
+        fun getInstance(context: Context): AppDb {
+            if (::INSTANCE.isInitialized) return INSTANCE
+            return synchronized(this) {
+                if (::INSTANCE.isInitialized) INSTANCE
+                else Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDb::class.java,
+                    DATABASE_NAME
+                ).fallbackToDestructiveMigration().build().also { INSTANCE = it }
+            }
+        }
+    }
+
+    abstract fun daoYourLifeDb(): DaoYourLifeDb
+
+    abstract fun daoTopicDb(): DaoTopicDb
+
+    abstract fun daoIdeaDb(): DaoIdeaDb
+
+    abstract fun daoSituationDayDb(): DaoSituationDayDb
+}
