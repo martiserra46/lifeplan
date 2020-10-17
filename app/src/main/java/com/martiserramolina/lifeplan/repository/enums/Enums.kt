29c3ba31b67c165c221880149931e131d96ec01c
martiserra46 <martiserra385@gmail.com>
2020-10-17 11:17:53 +0200

    Setup enum classes in repository

---
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
@@ -0,0 +1,5 @@
+package com.martiserramolina.lifeplan.repository.enums
+
+enum class IdeaImportance { IMPORTANT, NORMAL, UNIMPORTANT }
+
+enum class DaySituationSatisfaction { SATISFIED, NORMAL, UNSATISFIED }
