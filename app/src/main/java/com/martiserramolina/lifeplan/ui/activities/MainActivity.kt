9798d414927683cd50525ed4226c5d4a12b35e7f
martiserra46 <martiserra385@gmail.com>
2020-10-17 12:31:55 +0200

    Add navController property in MainActivity

--- app/src/main/java/com/martiserramolina/lifeplan/ui/activities/MainActivity.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/activities/MainActivity.kt
@@ -2,9 +2,17 @@ package com.martiserramolina.lifeplan.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.martiserramolina.lifeplan.R
 
 class MainActivity : AppCompatActivity() {
+
+    val navController: NavController
+        get() = supportFragmentManager.findFragmentById(
+            R.id.activity_main_fcv
+        ).run { this as NavHostFragment }.navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
