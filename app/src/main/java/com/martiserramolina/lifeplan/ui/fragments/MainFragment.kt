982e072e75db017e4022448228ee373f4e39cc09
martiserra46 <martiserra385@gmail.com>
2020-10-17 09:09:58 +0200

    Add navController property to MainFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/MainFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+
+    private val navController: NavController
+        get() = childFragmentManager.findFragmentById(
+            R.id.fragment_main_fcv
+        ).run { this as NavHostFragment }.navController
 
     override fun onCreateView(
         inflater: LayoutInflater,
