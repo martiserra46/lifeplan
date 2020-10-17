7ed8347c02cdf620b8f35af6cf7fd55b8c525cd5
martiserra46 <martiserra385@gmail.com>
2020-10-17 16:32:40 +0200

    Setup viewModel property in IdeasFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/ideas/IdeasFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/ideas/IdeasFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasBinding
 import com.martiserramolina.lifeplan.ui.adapters.TopicAdapter
+import com.martiserramolina.lifeplan.viewmodels.ideas.IdeasViewModel
 
 class IdeasFragment : Fragment() {
 
     private lateinit var binding: FragmentNavIdeasBinding
+    private val viewModel by lazy { buildViewModel() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +30,12 @@ class IdeasFragment : Fragment() {
             setHasFixedSize(true)
             adapter = TopicAdapter()
         }
+
+    }
+
+    private fun buildViewModel(): IdeasViewModel {
+        return ViewModelProvider(
+            this, IdeasViewModel.Factory(requireActivity().application)
+        ).get(IdeasViewModel::class.java)
     }
 }
