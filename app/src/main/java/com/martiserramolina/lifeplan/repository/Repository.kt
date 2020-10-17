ee49b8c593305f9837b3c6d9140a17daeed0c44d
martiserra46 <martiserra385@gmail.com>
2020-10-17 11:58:53 +0200

    Setup YourLifeRepository

--- app/src/main/java/com/martiserramolina/lifeplan/repository/Repository.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/Repository.kt
@@ -1,6 +1,10 @@
 package com.martiserramolina.lifeplan.repository
 
 import android.content.Context
+import com.martiserramolina.lifeplan.repository.model.YourLife
+import com.martiserramolina.lifeplan.repository.room.AppDb
+import com.martiserramolina.lifeplan.repository.room.YourLifeDb
+import com.martiserramolina.lifeplan.repository.room.toYourLife
 
 class Repository(context: Context) {
     val yourLifeRepository by lazy { YourLifeRepository(context) }
@@ -10,6 +14,16 @@ class Repository(context: Context) {
 
 class YourLifeRepository(context: Context) {
 
+    private val db by lazy { AppDb.getInstance(context) }
+    private val daoYourLife by lazy { db.daoYourLifeDb() }
+
+    suspend fun getYourLife(): YourLife? {
+        return daoYourLife.getYourLife()?.toYourLife()
+    }
+
+    suspend fun insertYourLife(lifeDescription: YourLife) {
+        daoYourLife.insertYourLife(YourLifeDb(1, lifeDescription.text))
+    }
 }
 
 class IdeasRepository(context: Context) {
