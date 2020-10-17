03ba3ec3090c779f062f16955bd8f811292bc800
martiserra46 <martiserra385@gmail.com>
2020-10-17 12:15:12 +0200

    Create viewModel in YourLifeFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/your_life/YourLifeFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/your_life/YourLifeFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.databinding.FragmentNavYourLifeBinding
+import com.martiserramolina.lifeplan.viewmodels.your_life.YourLifeViewModel
 
 class YourLifeFragment : Fragment() {
 
     private lateinit var binding: FragmentNavYourLifeBinding
+    private val viewModel by lazy { buildViewModel() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +21,13 @@ class YourLifeFragment : Fragment() {
     ): View? {
         binding = FragmentNavYourLifeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    
+    private fun buildViewModel(): YourLifeViewModel {
+        return ViewModelProvider(
+            this,
+            YourLifeViewModel.Factory(
+                requireActivity().application)
+        ).get(YourLifeViewModel::class.java)
     }
 }
