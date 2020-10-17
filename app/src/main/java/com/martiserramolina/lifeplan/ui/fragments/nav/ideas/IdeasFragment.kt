a5e94cc92eff65e4e18fb8c041ec034c189f7804
martiserra46 <martiserra385@gmail.com>
2020-10-17 16:57:55 +0200

    Setup add menu icon functionality in IdeasFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/ideas/IdeasFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/ideas/IdeasFragment.kt
@@ -1,18 +1,22 @@
 package com.martiserramolina.lifeplan.ui.fragments.nav.ideas
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasBinding
+import com.martiserramolina.lifeplan.ui.activities.MainActivity
 import com.martiserramolina.lifeplan.ui.adapters.TopicAdapter
+import com.martiserramolina.lifeplan.ui.fragments.MainFragmentDirections
+import com.martiserramolina.lifeplan.ui.fragments.nav.ideas.idea.IdeaFragmentDirections
+import com.martiserramolina.lifeplan.ui.fragments.nav.ideas.topic.TopicFragmentDirections
 import com.martiserramolina.lifeplan.viewmodels.ideas.IdeasViewModel
 
 class IdeasFragment : Fragment() {
 
     private lateinit var binding: FragmentNavIdeasBinding
+    private val mainActivity by lazy { activity as MainActivity }
     private val viewModel by lazy { buildViewModel() }
 
     override fun onCreateView(
@@ -26,7 +30,22 @@ class IdeasFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         setupRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.ideas_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.ideas_topic_add_mi -> {
+                navigateToAddTopicFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun buildViewModel(): IdeasViewModel {
@@ -43,5 +62,10 @@ class IdeasFragment : Fragment() {
         viewModel.topics.observe(viewLifecycleOwner) { topics ->
             binding.fragmentNavIdeasRv.adapter.run { this as TopicAdapter }.listTopics = topics
         }
+    }
+
+    private fun navigateToAddTopicFragment() {
+        mainActivity.navController
+            .navigate(MainFragmentDirections.actionMainFragmentToAddTopicFragment())
     }
 }
