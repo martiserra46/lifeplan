f1c9b20ceed73e897f97944672e7ea458d6ba92b
martiserra46 <martiserra385@gmail.com>
2020-10-17 17:42:23 +0200

    Setup adapter to recycler view in SituationFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
@@ -1,10 +1,12 @@
 package com.martiserramolina.lifeplan.ui.fragments.nav.situation
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationBinding
 import com.martiserramolina.lifeplan.ui.fragments.FragmentWithBinding
+import com.martiserramolina.lifeplan.ui.adapters.SituationDayAdapter
 
 class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
 
@@ -16,4 +18,10 @@ class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
     }
 
     override fun getRootView(): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentNavSituationRv.adapter = SituationDayAdapter()
+    }
+
 }
