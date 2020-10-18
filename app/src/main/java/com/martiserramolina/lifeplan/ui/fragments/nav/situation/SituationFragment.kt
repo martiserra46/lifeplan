ecef06b19828d8b5a0b904956bc43c379531b589
martiserra46 <martiserra385@gmail.com>
2020-10-18 11:58:29 +0200

    Setup loading situation days to recycler view functionality

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
@@ -2,16 +2,24 @@ package com.martiserramolina.lifeplan.ui.fragments.nav.situation
 
 import android.os.Bundle
 import android.view.*
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationBinding
 import com.martiserramolina.lifeplan.ui.activities.MainActivity
 import com.martiserramolina.lifeplan.ui.fragments.FragmentWithBinding
 import com.martiserramolina.lifeplan.ui.adapters.SituationDayAdapter
 import com.martiserramolina.lifeplan.ui.fragments.MainFragmentDirections
+import com.martiserramolina.lifeplan.viewmodels.situation.SituationViewModel
 
 class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
 
     private val mainActivity by lazy { activity as MainActivity }
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this, SituationViewModel.Factory(requireActivity().application)
+        ).get(SituationViewModel::class.java)
+    }
 
     override fun getBinding(
         inflater: LayoutInflater,
@@ -25,7 +33,7 @@ class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
-        binding.fragmentNavSituationRv.adapter = SituationDayAdapter()
+        setupRecyclerView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -39,6 +47,14 @@ class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.fragmentNavSituationRv.adapter = SituationDayAdapter()
+        viewModel.situationDays.observe(viewLifecycleOwner) { situationDays ->
+            binding.fragmentNavSituationRv.adapter
+                .run { this as SituationDayAdapter }.listSituationDays = situationDays
         }
     }
 
