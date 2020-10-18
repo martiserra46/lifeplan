ac578a6ca3f62d1c2d0634caf598cfe6c06b6a5c
martiserra46 <martiserra385@gmail.com>
2020-10-18 15:56:09 +0200

    Setup spinner functionality in AddSituationDayFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/add/AddSituationDayFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/add/AddSituationDayFragment.kt
@@ -1,8 +1,14 @@
 package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.situation.day.add
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationDaySaveBinding
 import com.martiserramolina.lifeplan.enums.NavSection
@@ -25,5 +31,38 @@ class AddSituationDayFragment : SecondaryFragment<FragmentNavSituationDaySaveBin
             AddSituationDayFragmentDirections
                 .actionAddSituationDayFragmentToMainFragment(NavSection.SITUATION)
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSatisfactionSpinner()
+    }
+
+    private fun setupSatisfactionSpinner() {
+        binding.fragmentNavSituationDaySaveSatisfactionSp.apply {
+            adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.situationDaySatisfactions,
+                android.R.layout.simple_spinner_item
+            ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    adapterView: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    view?.findViewById<TextView>(android.R.id.text1)?.apply {
+                        val colorResource = when (text) {
+                            getString(R.string.high_satisfaction) ->
+                                R.color.colorSituationDayHighSatisfaction
+                            getString(R.string.normal_satisfaction) ->
+                                R.color.colorSituationDayNormalSatisfaction
+                            else -> R.color.colorSituationDayLowSatisfaction
+                        }
+                        setTextColor(ContextCompat.getColor(context, colorResource))
+                    }
+                }
+
+                override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+            }
+        }
     }
 }
