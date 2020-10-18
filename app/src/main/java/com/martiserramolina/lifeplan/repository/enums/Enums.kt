c2a84d7449f3a254c4b9720935ef91859a9cfaba
martiserra46 <martiserra385@gmail.com>
2020-10-18 16:28:31 +0200

    Add properties to SituationDaySatisfaction enum class

--- app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
@@ -1,5 +1,28 @@
 package com.martiserramolina.lifeplan.repository.enums
 
+import android.content.Context
+import com.martiserramolina.lifeplan.R
+
 enum class IdeaImportance { IMPORTANT, NORMAL, UNIMPORTANT }
 
-enum class SituationDaySatisfaction { SATISFIED, NORMAL, UNSATISFIED }
+enum class SituationDaySatisfaction(val colorId: Int, val stringId: Int) {
+    SATISFIED(R.color.colorSituationDayHighSatisfaction, R.string.high_satisfaction),
+    NORMAL(R.color.colorSituationDayNormalSatisfaction, R.string.normal_satisfaction),
+    UNSATISFIED(R.color.colorSituationDayLowSatisfaction, R.string.low_satisfaction);
+
+    companion object {
+        fun getSituationDaySatisfactionByColorId(colorId: Int): SituationDaySatisfaction {
+            return SituationDaySatisfaction.values().first { it.colorId == colorId }
+        }
+
+        fun getSituationDaySatisfactionByStringId(stringId: Int): SituationDaySatisfaction {
+            return SituationDaySatisfaction.values().first { it.stringId == stringId }
+        }
+
+        fun getSituationDaySatisfactionByString(
+            context: Context, string: String
+        ): SituationDaySatisfaction {
+            return SituationDaySatisfaction.values().first { context.getString(it.stringId) == string }
+        }
+    }
+}
