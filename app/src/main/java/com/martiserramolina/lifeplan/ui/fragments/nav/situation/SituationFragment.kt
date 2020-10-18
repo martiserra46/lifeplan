2826c275bbc9f1245f8828eee74bf10c9b6db576
martiserra46 <martiserra385@gmail.com>
2020-10-18 11:32:11 +0200

    Add functionality to navigate to AddSituationDayFragmentin SituationFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/situation/SituationFragment.kt
@@ -1,14 +1,17 @@
 package com.martiserramolina.lifeplan.ui.fragments.nav.situation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationBinding
+import com.martiserramolina.lifeplan.ui.activities.MainActivity
 import com.martiserramolina.lifeplan.ui.fragments.FragmentWithBinding
 import com.martiserramolina.lifeplan.ui.adapters.SituationDayAdapter
+import com.martiserramolina.lifeplan.ui.fragments.MainFragmentDirections
 
 class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
+
+    private val mainActivity by lazy { activity as MainActivity }
 
     override fun getBinding(
         inflater: LayoutInflater,
@@ -21,7 +24,26 @@ class SituationFragment : FragmentWithBinding<FragmentNavSituationBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         binding.fragmentNavSituationRv.adapter = SituationDayAdapter()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.situation_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.situation_add_mi -> {
+                navigateToAddSituationDayFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigateToAddSituationDayFragment() {
+        mainActivity.navController
+            .navigate(MainFragmentDirections.actionMainFragmentToAddSituationDayFragment())
+    }
 }
