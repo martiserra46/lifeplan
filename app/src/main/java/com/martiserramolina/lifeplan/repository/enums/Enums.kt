8e23550d76f009e3f9d88a67e43176117486436f
martiserra46 <martiserra385@gmail.com>
2020-10-19 11:39:57 +0200

    Remove unnecessary methods in SituationDaySatisfaction

--- app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
@@ -1,6 +1,5 @@
 package com.martiserramolina.lifeplan.repository.enums
 
-import android.content.Context
 import com.martiserramolina.lifeplan.R
 
 enum class IdeaImportance { IMPORTANT, NORMAL, UNIMPORTANT }
@@ -20,21 +19,5 @@ enum class SituationDaySatisfaction(val drawableId: Int, val colorId: Int, val s
         R.drawable.bg_rvi_situation_day_circle_unsatisfied,
         R.color.colorSituationDayLowSatisfaction,
         R.string.low_satisfaction
-    );
-
-    companion object {
-        fun getSituationDaySatisfactionByColorId(colorId: Int): SituationDaySatisfaction {
-            return SituationDaySatisfaction.values().first { it.colorId == colorId }
-        }
-
-        fun getSituationDaySatisfactionByStringId(stringId: Int): SituationDaySatisfaction {
-            return SituationDaySatisfaction.values().first { it.stringId == stringId }
-        }
-
-        fun getSituationDaySatisfactionByString(
-            context: Context, string: String
-        ): SituationDaySatisfaction {
-            return SituationDaySatisfaction.values().first { context.getString(it.stringId) == string }
-        }
-    }
+    )
 }
