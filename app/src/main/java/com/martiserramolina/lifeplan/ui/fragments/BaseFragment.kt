9fd77fd65bb60a06214d338cf68ce893e166585d
martiserra46 <martiserra385@gmail.com>
2020-10-19 11:16:13 +0200

    Add hideKeyboard functionality in BaseFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/BaseFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/BaseFragment.kt
@@ -1,11 +1,14 @@
 package com.martiserramolina.lifeplan.ui.fragments
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.extensions.hideKeyboard
 
 abstract class BaseFragment<T : ViewBinding> : Fragment() {
 
@@ -20,5 +23,12 @@ abstract class BaseFragment<T : ViewBinding> : Fragment() {
         return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        hideKeyboard()
+    }
+
     abstract fun getBinding(inflater: LayoutInflater, container: ViewGroup?): T
+
+    private fun hideKeyboard() { requireActivity().hideKeyboard() }
 }
