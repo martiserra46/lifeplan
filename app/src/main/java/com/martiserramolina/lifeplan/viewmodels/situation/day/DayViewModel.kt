689e4ab0fbf964488c73867ae0bde5b3bcfa2e4c
martiserra46 <martiserra385@gmail.com>
2020-10-20 17:42:57 +0200

    Setup DayViewModel

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/day/DayViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/day/DayViewModel.kt
@@ -2,20 +2,16 @@ package com.martiserramolina.lifeplan.viewmodels.situation.day
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.martiserramolina.lifeplan.repository.model.Day
+import com.martiserramolina.lifeplan.repository.room.Day
 import java.lang.IllegalArgumentException
 
-class DayViewModel(
-    val dayId: Long, val day: Day
-) : ViewModel() {
+class DayViewModel(val day: Day) : ViewModel() {
 
-    class Factory(
-        private val dayId: Long, private val day: Day
-    ) : ViewModelProvider.Factory {
+    class Factory(private val day: Day) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(DayViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return DayViewModel(dayId, day) as T
+                return DayViewModel(day) as T
             }
             throw IllegalArgumentException("Invalid ViewModel")
         }
