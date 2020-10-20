37d4f0e29eacfe235f6804a414380c5dc42f4c46
martiserra46 <martiserra385@gmail.com>
2020-10-20 16:11:21 +0200

    Remove extension functions in Entities.kt

--- app/src/main/java/com/martiserramolina/lifeplan/repository/room/Entities.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/room/Entities.kt
@@ -20,14 +20,6 @@ data class LifeDb(
     val lifeText: String = ""
 )
 
-fun LifeDb.toLife(): Life {
-    return Life(lifeText)
-}
-
-fun Life.toLifeDb(): LifeDb {
-    return LifeDb(lifeText = text)
-}
-
 @Entity(tableName = "topic")
 data class TopicDb(
     @PrimaryKey(autoGenerate = true)
@@ -36,22 +28,6 @@ data class TopicDb(
     @ColumnInfo(name = "topic_name")
     val topicName: String = ""
 )
-
-fun TopicDb.toTopic(): Topic {
-    return Topic(topicName)
-}
-
-fun List<TopicDb>.toListTopics(): List<Topic> {
-    return map { it.toTopic() }
-}
-
-fun Topic.toTopicDb(): TopicDb {
-    return TopicDb(0, name)
-}
-
-fun List<Topic>.toListTopicsDb(): List<TopicDb> {
-    return map { it.toTopicDb() }
-}
 
 @Entity(tableName = "idea")
 data class IdeaDb(
@@ -68,22 +44,6 @@ data class IdeaDb(
     var ideaLastTimeModified: Date = Date()
 )
 
-fun IdeaDb.toIdea(): Idea {
-    return Idea(ideaText, ideaImportance, ideaLastTimeModified)
-}
-
-fun List<IdeaDb>.toListIdeas(): List<Idea> {
-    return map { it.toIdea() }
-}
-
-fun Idea.toIdeaDb(topicId: Long): IdeaDb {
-    return IdeaDb(0, topicId, text, importance, lastTimeModified)
-}
-
-fun List<Idea>.toListIdeasDb(topicId: Long): List<IdeaDb> {
-    return map { it.toIdeaDb(topicId) }
-}
-
 @Entity(tableName = "day")
 data class DayDb(
     @PrimaryKey(autoGenerate = true)
@@ -96,19 +56,3 @@ data class DayDb(
     @ColumnInfo(name = "day_satisfaction")
     var daySatisfaction: DaySatisfaction = DaySatisfaction.NORMAL,
 )
-
-fun DayDb.toDay(): Pair<Long, Day> {
-    return dayId to Day(dayDate, dayText, daySatisfaction)
-}
-
-fun List<DayDb>.toListDays(): List<Pair<Long, Day>> {
-    return map { it.toDay() }
-}
-
-fun Day.toDayDb(id: Long = 0): DayDb {
-    return DayDb(id, date, text, satisfaction)
-}
-
-fun List<Day>.toListDaysDb(): List<DayDb> {
-    return map { it.toDayDb() }
-}
