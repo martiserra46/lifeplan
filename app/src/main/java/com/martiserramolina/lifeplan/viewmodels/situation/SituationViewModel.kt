6c15ee35ec82f904fa8e301f9ebc1cdd29dc06cf
martiserra46 <martiserra385@gmail.com>
2020-10-20 17:37:53 +0200

    Setup SituationViewModel

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/SituationViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/SituationViewModel.kt
@@ -1,20 +1,26 @@
 package com.martiserramolina.lifeplan.viewmodels.situation
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.martiserramolina.lifeplan.repository.Repository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import androidx.lifecycle.*
+import com.martiserramolina.lifeplan.repository.SituationRepository
+import com.martiserramolina.lifeplan.repository.room.AppDb
+import com.martiserramolina.lifeplan.repository.room.Day
+import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
 class SituationViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = Repository(application.applicationContext)
+    private val repository by lazy {
+        SituationRepository(AppDb.getInstance(application.applicationContext).daoSituation())
+    }
 
-    val days = repository.dayRepository.getDays()
+    val days = MutableLiveData<List<Day>>()
+
+    init {
+        viewModelScope.launch {
+            days.value = repository.getDays()
+        }
+    }
 
     class Factory(private val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
