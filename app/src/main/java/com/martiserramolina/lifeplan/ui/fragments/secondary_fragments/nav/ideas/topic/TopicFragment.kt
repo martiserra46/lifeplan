533fbc870081eb3df168a11db32531a50162911f
martiserra46 <martiserra385@gmail.com>
2020-10-21 11:43:43 +0200

    Setup TopicFragment extending SecondaryFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/TopicFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/TopicFragment.kt
@@ -2,12 +2,28 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasTopicBinding
-import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.enums.NavSection
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 
-class TopicFragment : BaseFragment<ViewBinding>() {
-    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
+class TopicFragment : SecondaryFragment<FragmentNavIdeasTopicBinding>() {
+
+    override fun getBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentNavIdeasTopicBinding {
         return FragmentNavIdeasTopicBinding.inflate(inflater, container, false)
+    }
+
+    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasTopicTb
+
+    override fun getToolbarTitle(): String = ""
+
+    override fun navigateToPreviousFragment() {
+        navController
+            .navigate(TopicFragmentDirections.actionTopicFragmentToMainFragment(NavSection.IDEAS))
     }
 }
