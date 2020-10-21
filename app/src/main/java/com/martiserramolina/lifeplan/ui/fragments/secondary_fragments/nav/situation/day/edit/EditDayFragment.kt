0178eb8a40545c7054d4f41adfc911419472a8b2
martiserra46 <martiserra385@gmail.com>
2020-10-21 10:19:19 +0200

    Setup EditDayFragment making it to extend SecondaryFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
@@ -2,10 +2,12 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.situa
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationDaySaveBinding
-import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 
-class EditDayFragment : BaseFragment<FragmentNavSituationDaySaveBinding>() {
+class EditDayFragment : SecondaryFragment<FragmentNavSituationDaySaveBinding>() {
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -13,4 +15,11 @@ class EditDayFragment : BaseFragment<FragmentNavSituationDaySaveBinding>() {
         return FragmentNavSituationDaySaveBinding.inflate(inflater, container, false)
     }
 
+    override fun getToolbar(): Toolbar = binding.fragmentNavSituationDaySaveTb
+
+    override fun getToolbarTitle(): String = getString(R.string.edit)
+
+    override fun navigateToPreviousFragment() {
+        navController.navigateUp()
+    }
 }
