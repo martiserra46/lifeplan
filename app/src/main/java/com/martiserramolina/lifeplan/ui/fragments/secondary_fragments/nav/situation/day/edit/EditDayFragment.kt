8be5f4f31f2224addc69dada30d490adffec71ab
martiserra46 <martiserra385@gmail.com>
2020-10-21 10:25:13 +0200

    Add viewModel to EditDayFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
@@ -3,11 +3,25 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.situa
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationDaySaveBinding
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
+import com.martiserramolina.lifeplan.viewmodels.situation.day.add.AddDayViewModel
+import com.martiserramolina.lifeplan.viewmodels.situation.day.edit.EditDayViewModel
 
 class EditDayFragment : SecondaryFragment<FragmentNavSituationDaySaveBinding>() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            EditDayViewModel.Factory(
+                EditDayFragmentArgs.fromBundle(requireArguments()).day,
+                requireActivity().application
+            )
+        ).get(EditDayViewModel::class.java)
+    }
+
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
