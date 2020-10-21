4b742999c2fa5f44dfcbfd18a640ec7dcfe0ad47
martiserra46 <martiserra385@gmail.com>
2020-10-21 10:23:10 +0200

    Modify EditDayViewModel creating day constructor argument

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/day/edit/EditDayViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/situation/day/edit/EditDayViewModel.kt
@@ -12,13 +12,11 @@ import kotlinx.coroutines.*
 import java.lang.IllegalArgumentException
 import java.util.*
 
-class EditDayViewModel(application: Application) : AndroidViewModel(application) {
+class EditDayViewModel(val day: Day, application: Application) : AndroidViewModel(application) {
 
     private val repository by lazy {
         SituationRepository(AppDb.getInstance(application.applicationContext).daoSituation())
     }
-
-    lateinit var date: Date
 
     fun updateDay(day: Day) {
         viewModelScope.launch {
@@ -26,11 +24,13 @@ class EditDayViewModel(application: Application) : AndroidViewModel(application)
         }
     }
 
-    class Factory(private val application: Application) : ViewModelProvider.Factory {
+    class Factory(
+        private val day: Day, private val application: Application
+    ) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(EditDayViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return EditDayViewModel(application) as T
+                return EditDayViewModel(day, application) as T
             }
             throw IllegalArgumentException("Invalid ViewModel")
         }
