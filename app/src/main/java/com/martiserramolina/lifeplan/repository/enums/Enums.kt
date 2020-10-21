7ce608a6abbc9f60f6727bbb625db36bb07ba943
martiserra46 <martiserra385@gmail.com>
2020-10-21 12:44:39 +0200

    Setup IdeaImportance with attributes

--- app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/repository/enums/Enums.kt
@@ -2,7 +2,23 @@ package com.martiserramolina.lifeplan.repository.enums
 
 import com.martiserramolina.lifeplan.R
 
-enum class IdeaImportance { IMPORTANT, NORMAL, UNIMPORTANT }
+enum class IdeaImportance(val drawableId: Int, val colorId: Int, val stringId: Int) {
+    IMPORTANT(
+        R.drawable.bg_rvi_ideas_idea_circle_important,
+        R.color.colorIdeaHighImportance,
+        R.string.high_importance
+    ),
+    NORMAL(
+        R.drawable.bg_rvi_ideas_idea_circle_normal,
+        R.color.colorIdeaNormalImportance,
+        R.string.normal_importance
+    ),
+    UNIMPORTANT(
+        R.drawable.bg_rvi_ideas_idea_circle_unimportant,
+        R.color.colorIdeaLowImportance,
+        R.string.low_importance
+    )
+}
 
 enum class DaySatisfaction(val drawableId: Int, val colorId: Int, val stringId: Int) {
     SATISFIED(
