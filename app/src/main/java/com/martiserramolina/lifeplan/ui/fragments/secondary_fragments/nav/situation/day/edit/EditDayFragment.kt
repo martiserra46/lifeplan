9c45b8769279b04a745245ab99c387048ba6e5d6
martiserra46 <martiserra385@gmail.com>
2020-10-21 10:35:59 +0200

    Setup onViewCreated() functionality in EditDayFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/situation/day/edit/EditDayFragment.kt
@@ -1,11 +1,16 @@
 package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.situation.day.edit
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavSituationDaySaveBinding
+import com.martiserramolina.lifeplan.extensions.format
+import com.martiserramolina.lifeplan.repository.enums.DaySatisfaction
+import com.martiserramolina.lifeplan.ui.adapters.DaySatisfactionAdapter
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 import com.martiserramolina.lifeplan.viewmodels.situation.day.add.AddDayViewModel
 import com.martiserramolina.lifeplan.viewmodels.situation.day.edit.EditDayViewModel
@@ -35,5 +40,29 @@ class EditDayFragment : SecondaryFragment<FragmentNavSituationDaySaveBinding>() 
 
     override fun navigateToPreviousFragment() {
         navController.navigateUp()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupDateTv()
+        setupSatisfactionSp()
+        setupTextTv()
+    }
+
+    private fun setupDateTv() {
+        binding.fragmentNavSituationDaySaveDateTv.text = viewModel.day.dayDate.format("dd/mm/yyyy")
+    }
+
+    private fun setupSatisfactionSp() {
+        binding.fragmentNavSituationDaySaveSatisfactionSp.apply {
+            adapter = DaySatisfactionAdapter(
+                requireContext(), R.layout.spinner_item
+            ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+            setSelection(viewModel.day.daySatisfaction.ordinal)
+        }
+    }
+
+    private fun setupTextTv() {
+        binding.fragmentNavSituationDaySaveDateTv.text = viewModel.day.dayText
     }
 }
