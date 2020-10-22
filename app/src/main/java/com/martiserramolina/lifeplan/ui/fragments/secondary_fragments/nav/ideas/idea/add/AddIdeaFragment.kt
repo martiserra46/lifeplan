57c328ff89471106568cd9d98d60698a5da8395f
martiserra46 <martiserra385@gmail.com>
2020-10-22 11:06:09 +0200

    Setup AddIdeaFragment main functionality

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/add/AddIdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/add/AddIdeaFragment.kt
@@ -2,14 +2,39 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaSaveBinding
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
+import com.martiserramolina.lifeplan.viewmodels.ideas.idea.add.AddIdeaViewModel
 
-class AddIdeaFragment : BaseFragment<FragmentNavIdeasIdeaSaveBinding>() {
+class AddIdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaSaveBinding>() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            AddIdeaViewModel.Factory(
+                AddIdeaFragmentArgs.fromBundle(requireArguments()).topic,
+                requireActivity().application)
+        ).get(AddIdeaViewModel::class.java)
+    }
+
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentNavIdeasIdeaSaveBinding {
         return FragmentNavIdeasIdeaSaveBinding.inflate(inflater, container, false)
+    }
+
+    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasIdeaSaveTb
+
+    override fun getToolbarTitle(): String = getString(R.string.add_idea)
+
+    override fun navigateToPreviousFragment() {
+        navController.navigate(
+            AddIdeaFragmentDirections.actionAddIdeaFragmentToTopicFragment(viewModel.topic)
+        )
     }
 }
