2077c506b6d3f4a424cb0c812700aa397a9ec448
martiserra46 <martiserra385@gmail.com>
2020-10-22 11:02:51 +0200

    Setup AddIdeaViewModel

---
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/ideas/idea/add/AddIdeaViewModel.kt
@@ -0,0 +1,38 @@
+package com.martiserramolina.lifeplan.viewmodels.ideas.idea.add
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.martiserramolina.lifeplan.repository.IdeasRepository
+import com.martiserramolina.lifeplan.repository.room.AppDb
+import com.martiserramolina.lifeplan.repository.room.Idea
+import com.martiserramolina.lifeplan.repository.room.Topic
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class AddIdeaViewModel(val topic: Topic, application: Application) : AndroidViewModel(application) {
+
+    private val repository by lazy {
+        IdeasRepository(AppDb.getInstance(application.applicationContext).daoIdeas())
+    }
+
+    fun insertIdea(idea: Idea) {
+        viewModelScope.launch {
+            repository.insertIdea(idea)
+        }
+    }
+
+    class Factory(
+        private val topic: Topic, private val application: Application
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(AddIdeaViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return AddIdeaViewModel(topic, application) as T
+            }
+            throw IllegalArgumentException("Invalid ViewModel")
+        }
+    }
+}
