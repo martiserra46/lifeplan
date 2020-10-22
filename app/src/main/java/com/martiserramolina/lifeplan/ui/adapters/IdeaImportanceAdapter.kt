292eef25e93364f0ec3352b33554b8cf9879bf75
martiserra46 <martiserra385@gmail.com>
2020-10-22 11:12:17 +0200

    Setup IdeaImportanceAdapter

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/IdeaImportanceAdapter.kt
@@ -0,0 +1,29 @@
+package com.martiserramolina.lifeplan.ui.adapters
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.martiserramolina.lifeplan.repository.enums.IdeaImportance
+
+class IdeaImportanceAdapter(
+    context: Context, textViewId: Int
+) : ArrayAdapter<IdeaImportance>(context, textViewId, IdeaImportance.values()) {
+
+    private val listImportanceLevels = IdeaImportance.values()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val textView = super.getView(position, convertView, parent) as TextView
+        return textView.apply {
+            text = context.getString(listImportanceLevels[position].stringId)
+            setTextColor(ContextCompat.getColor(context, listImportanceLevels[position].colorId))
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val textView = super.getDropDownView(position, convertView, parent) as TextView
+        return textView.apply { text = context.getString(listImportanceLevels[position].stringId) }
+    }
+}
