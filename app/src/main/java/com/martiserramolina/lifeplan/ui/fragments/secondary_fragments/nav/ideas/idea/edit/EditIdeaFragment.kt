0256c49c801a73e8bfa70c6ba7e049bbbe5c03b4
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:55:19 +0200

    Setup onViewCreated functionality in EditIdeaFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/edit/EditIdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/edit/EditIdeaFragment.kt
@@ -1,10 +1,16 @@
 package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas.idea.edit
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaSaveBinding
+import com.martiserramolina.lifeplan.ui.adapters.DaySatisfactionAdapter
+import com.martiserramolina.lifeplan.ui.adapters.IdeaImportanceAdapter
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas.idea.IdeaFragmentArgs
 import com.martiserramolina.lifeplan.viewmodels.ideas.idea.edit.EditIdeaViewModel
@@ -35,5 +41,29 @@ class EditIdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaSaveBinding>() {
             EditIdeaFragmentDirections
                 .actionEditIdeaFragmentToIdeaFragment(viewModel.idea, viewModel.topic)
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTitleTv()
+        setupImportanceSp()
+        setupDescriptionTv()
+    }
+
+    private fun setupTitleTv() {
+        binding.fragmentNavIdeasIdeaSaveTitleEt.setText(viewModel.idea.ideaTitle)
+    }
+
+    private fun setupImportanceSp() {
+        binding.fragmentNavIdeasIdeaSaveImportanceSp.apply {
+            adapter = IdeaImportanceAdapter(
+                requireContext(), R.layout.spinner_item
+            ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+            setSelection(viewModel.idea.ideaImportance.ordinal)
+        }
+    }
+
+    private fun setupDescriptionTv() {
+        binding.fragmentNavIdeasIdeaSaveDescriptionEt.setText(viewModel.idea.ideaDescription)
     }
 }
