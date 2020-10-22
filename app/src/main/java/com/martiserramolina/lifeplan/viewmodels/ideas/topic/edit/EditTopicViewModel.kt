ac1dc2bd0514b6c0b772782c7d602488ecbc0323
martiserra46 <martiserra385@gmail.com>
2020-10-22 10:34:47 +0200

    Setup EditTopicViewModel

---
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/ideas/topic/edit/EditTopicViewModel.kt
@@ -0,0 +1,37 @@
+package com.martiserramolina.lifeplan.viewmodels.ideas.topic.edit
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.martiserramolina.lifeplan.repository.IdeasRepository
+import com.martiserramolina.lifeplan.repository.room.AppDb
+import com.martiserramolina.lifeplan.repository.room.Topic
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class EditTopicViewModel(val topic: Topic, application: Application) : AndroidViewModel(application) {
+
+    private val repository by lazy {
+        IdeasRepository(AppDb.getInstance(application.applicationContext).daoIdeas())
+    }
+
+    fun updateTopic() {
+        viewModelScope.launch {
+            repository.updateTopic(topic)
+        }
+    }
+
+    class Factory(
+        private val topic: Topic, private val application: Application
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(EditTopicViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return EditTopicViewModel(topic, application) as T
+            }
+            throw IllegalArgumentException("Invalid ViewModel")
+        }
+    }
+}
