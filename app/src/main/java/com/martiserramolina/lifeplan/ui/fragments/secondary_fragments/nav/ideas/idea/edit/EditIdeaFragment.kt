4e06110f5116d12739b7ba212fb83fa3046f39ad
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:51:01 +0200

    Setup EditIdeaFragment main functionality

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/edit/EditIdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/edit/EditIdeaFragment.kt
@@ -2,10 +2,23 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaSaveBinding
-import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas.idea.IdeaFragmentArgs
+import com.martiserramolina.lifeplan.viewmodels.ideas.idea.edit.EditIdeaViewModel
 
-class EditIdeaFragment : BaseFragment<FragmentNavIdeasIdeaSaveBinding>() {
+class EditIdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaSaveBinding>() {
+
+    private val viewModel by lazy {
+        val args = IdeaFragmentArgs.fromBundle(requireArguments())
+        ViewModelProvider(
+            this,
+            EditIdeaViewModel.Factory(args.topic, args.idea, requireActivity().application)
+        ).get(EditIdeaViewModel::class.java)
+    }
+
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -13,4 +26,14 @@ class EditIdeaFragment : BaseFragment<FragmentNavIdeasIdeaSaveBinding>() {
         return FragmentNavIdeasIdeaSaveBinding.inflate(inflater, container, false)
     }
 
+    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasIdeaSaveTb
+
+    override fun getToolbarTitle(): String = ""
+
+    override fun navigateToPreviousFragment() {
+        navController.navigate(
+            EditIdeaFragmentDirections
+                .actionEditIdeaFragmentToIdeaFragment(viewModel.idea, viewModel.topic)
+        )
+    }
 }
