ad75826110b502a117cd202a44fa8440c9d70157
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:13:09 +0200

    Setup IdeaFragment main functionality

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/IdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/IdeaFragment.kt
@@ -2,15 +2,37 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaBinding
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
+import com.martiserramolina.lifeplan.viewmodels.ideas.idea.IdeaViewModel
 
-class IdeaFragment : BaseFragment<FragmentNavIdeasIdeaBinding>() {
+class IdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaBinding>() {
+
+    private val viewModel by lazy {
+        val args = IdeaFragmentArgs.fromBundle(requireArguments())
+        ViewModelProvider(
+            this,
+            IdeaViewModel.Factory(args.topic, args.idea, requireActivity().application)
+        ).get(IdeaViewModel::class.java)
+    }
+
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentNavIdeasIdeaBinding {
         return FragmentNavIdeasIdeaBinding.inflate(inflater, container, false)
+    }
+
+    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasIdeaTb
+
+    override fun getToolbarTitle(): String = ""
+
+    override fun navigateToPreviousFragment() {
+        navController
+            .navigate(IdeaFragmentDirections.actionIdeaFragmentToTopicFragment(viewModel.topic))
     }
 
 }
