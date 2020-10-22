9fe7ca6cc2d271ec245548da7a49c126f19bc409
martiserra46 <martiserra385@gmail.com>
2020-10-22 11:20:05 +0200

    Setup importance spinner in AddIdeaFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/add/AddIdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/add/AddIdeaFragment.kt
@@ -1,11 +1,15 @@
 package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas.idea.add
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaSaveBinding
+import com.martiserramolina.lifeplan.ui.adapters.DaySatisfactionAdapter
+import com.martiserramolina.lifeplan.ui.adapters.IdeaImportanceAdapter
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 import com.martiserramolina.lifeplan.viewmodels.ideas.idea.add.AddIdeaViewModel
@@ -36,5 +40,18 @@ class AddIdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaSaveBinding>() {
         navController.navigate(
             AddIdeaFragmentDirections.actionAddIdeaFragmentToTopicFragment(viewModel.topic)
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupImportanceSp()
+    }
+
+    private fun setupImportanceSp() {
+        binding.fragmentNavIdeasIdeaSaveImportanceSp.apply {
+            adapter = IdeaImportanceAdapter(
+                requireContext(), R.layout.spinner_item
+            ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+        }
     }
 }
