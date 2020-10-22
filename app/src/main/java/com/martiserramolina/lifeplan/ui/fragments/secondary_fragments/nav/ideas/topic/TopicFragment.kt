87d938426cdd80b6ea0cde9e9a5957112e5e74d7
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:16:41 +0200

    Add navigation to idea fragment functionality in TopicFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/TopicFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/TopicFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasTopicBinding
 import com.martiserramolina.lifeplan.enums.NavSection
+import com.martiserramolina.lifeplan.repository.room.Idea
 import com.martiserramolina.lifeplan.ui.adapters.IdeaAdapter
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 import com.martiserramolina.lifeplan.viewmodels.ideas.topic.TopicViewModel
@@ -76,7 +77,7 @@ class TopicFragment : SecondaryFragment<FragmentNavIdeasTopicBinding>() {
     private fun setupIdeasRv() {
         binding.fragmentNavIdeasTopicRv.apply {
             setHasFixedSize(true)
-            adapter = IdeaAdapter {}
+            adapter = IdeaAdapter { navigateToIdeaFragment(it) }
             addItemDecoration(
                 DividerItemDecoration(context, DividerItemDecoration.VERTICAL).apply {
                     setDrawable(ContextCompat.getDrawable(context, R.drawable.div_rvi)!!)
@@ -102,5 +103,11 @@ class TopicFragment : SecondaryFragment<FragmentNavIdeasTopicBinding>() {
     private fun deleteTopic() {
         viewModel.deleteTopic()
         navigateToPreviousFragment()
+    }
+
+    private fun navigateToIdeaFragment(idea: Idea) {
+        navController.navigate(
+            TopicFragmentDirections.actionTopicFragmentToIdeaFragment(idea, viewModel.topic)
+        )
     }
 }
