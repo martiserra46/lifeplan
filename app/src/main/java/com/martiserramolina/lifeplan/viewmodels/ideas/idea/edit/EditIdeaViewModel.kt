fba59946f1bab3d0f4925398b3af462100d7be4f
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:41:36 +0200

    Setup EditIdeaViewModel

---
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/ideas/idea/edit/EditIdeaViewModel.kt
@@ -0,0 +1,40 @@
+package com.martiserramolina.lifeplan.viewmodels.ideas.idea.edit
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.martiserramolina.lifeplan.repository.IdeasRepository
+import com.martiserramolina.lifeplan.repository.room.AppDb
+import com.martiserramolina.lifeplan.repository.room.Idea
+import com.martiserramolina.lifeplan.repository.room.Topic
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class EditIdeaViewModel(
+    val idea: Idea, val topic: Topic, application: Application
+) : AndroidViewModel(application) {
+
+    private val repository by lazy {
+        IdeasRepository(AppDb.getInstance(application.applicationContext).daoIdeas())
+    }
+
+    fun updateIdea(idea: Idea) {
+        viewModelScope.launch {
+            repository.updateIdea(idea)
+        }
+    }
+
+    class Factory(
+        private val topic: Topic, private val idea: Idea, private val application: Application
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(EditIdeaViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return EditIdeaViewModel(idea, topic, application) as T
+            }
+            throw IllegalArgumentException("Invalid ViewModel")
+        }
+    }
+}
