f3d08690bb42d090eec4da8c7e414e10a79f3ebc
martiserra46 <martiserra385@gmail.com>
2020-10-22 10:36:56 +0200

    Setup EditTopicFragment main functionality

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/edit/EditTopicFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/topic/edit/EditTopicFragment.kt
@@ -2,14 +2,39 @@ package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasTopicSaveBinding
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
+import com.martiserramolina.lifeplan.viewmodels.ideas.topic.edit.EditTopicViewModel
 
-class EditTopicFragment : BaseFragment<FragmentNavIdeasTopicSaveBinding>() {
+class EditTopicFragment : SecondaryFragment<FragmentNavIdeasTopicSaveBinding>() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            EditTopicViewModel.Factory(
+                EditTopicFragmentArgs.fromBundle(requireArguments()).topic,
+                requireActivity().application
+            )
+        ).get(EditTopicViewModel::class.java)
+    }
+    
     override fun getBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentNavIdeasTopicSaveBinding {
         return FragmentNavIdeasTopicSaveBinding.inflate(inflater, container, false)
+    }
+
+    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasTopicSaveTb
+
+    override fun getToolbarTitle(): String = getString(R.string.edit)
+
+    override fun navigateToPreviousFragment() {
+        navController
+            .navigate(EditTopicFragmentDirections.actionEditTopicFragmentToTopicFragment(viewModel.topic))
     }
 }
