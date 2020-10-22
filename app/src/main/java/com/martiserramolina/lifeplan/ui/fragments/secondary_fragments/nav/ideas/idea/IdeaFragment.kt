227249ca45fd06388cf7e6dfb9f62c2d8d55cee5
martiserra46 <martiserra385@gmail.com>
2020-10-22 12:23:47 +0200

    Setup onViewCreated functionality to IdeaFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/IdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/secondary_fragments/nav/ideas/idea/IdeaFragment.kt
@@ -1,11 +1,13 @@
 package com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.nav.ideas.idea
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaBinding
-import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
 import com.martiserramolina.lifeplan.ui.fragments.secondary_fragments.SecondaryFragment
 import com.martiserramolina.lifeplan.viewmodels.ideas.idea.IdeaViewModel
 
@@ -35,4 +37,27 @@ class IdeaFragment : SecondaryFragment<FragmentNavIdeasIdeaBinding>() {
             .navigate(IdeaFragmentDirections.actionIdeaFragmentToTopicFragment(viewModel.topic))
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTitleTv()
+        setupImportanceTv()
+        setupDescriptionTv()
+    }
+
+    private fun setupTitleTv() {
+        binding.fragmentNavIdeasIdeaTitleTv.text = viewModel.idea.ideaTitle
+    }
+
+    private fun setupImportanceTv() {
+        viewModel.idea.ideaImportance.let { importance ->
+            binding.fragmentNavIdeasIdeaImportanceTv.apply {
+                text = getString(importance.stringId)
+                setTextColor(ContextCompat.getColor(context, importance.colorId))
+            }
+        }
+    }
+
+    private fun setupDescriptionTv() {
+        binding.fragmentNavIdeasIdeaDescriptionTv.text = viewModel.idea.ideaDescription
+    }
 }
