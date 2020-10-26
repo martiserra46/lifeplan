370c7a8b49365174475a16b82958afde74341f34
martiserra46 <martiserra385@gmail.com>
2020-10-26 12:15:18 +0100

    Setup OnMenuItemClickListeners.kt

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/interfaces/OnMenuItemClickListeners.kt
@@ -0,0 +1,17 @@
+package com.martiserramolina.lifeplan.ui.fragments.interfaces
+
+interface OnDeleteMenuItemClickListener {
+    fun onDeleteMenuItemClicked(): Boolean
+}
+
+interface OnEditMenuItemClickListener {
+    fun onEditMenuItemClicked(): Boolean
+}
+
+interface OnAddMenuItemClickListener {
+    fun onAddMenuItemClicked(): Boolean
+}
+
+interface OnSaveMenuItemClickListener {
+    fun onSaveMenuItemClicked(): Boolean
+}
