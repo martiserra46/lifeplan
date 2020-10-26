66e088a8f187b99d75069143a0b8de9bc1659201
martiserra46 <martiserra385@gmail.com>
2020-10-26 11:10:08 +0100

    Setup SaveItemFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/abstracts/SaveItemFragment.kt
@@ -0,0 +1,38 @@
+package com.martiserramolina.lifeplan.ui.fragments.abstracts
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.viewbinding.ViewBinding
+
+abstract class SaveItemFragment<T : ViewBinding> : SecondaryFragment<T>() {
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(getMenuResource(), menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            getSaveMenuItemId() -> {
+                saveItemDataIfValid()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    abstract fun getMenuResource(): Int
+
+    abstract fun getSaveMenuItemId(): Int
+
+    private fun saveItemDataIfValid() {
+        if (isItemDataValid()) saveItemData()
+        else showInvalidDataMessage()
+    }
+
+    abstract fun isItemDataValid(): Boolean
+
+    abstract fun saveItemData()
+
+    abstract fun showInvalidDataMessage()
+}
