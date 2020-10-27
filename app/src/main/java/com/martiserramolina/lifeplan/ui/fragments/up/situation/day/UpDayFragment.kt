ef89e83eedb59b5c0a3a66661afb55cc18c16b81
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:58:01 +0100

    Create UpDayFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/situation/day/UpDayFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.up.situation.day
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.up.situation.UpSituationFragment
+
+abstract class UpDayFragment<T : ViewBinding> : UpSituationFragment<T>()
