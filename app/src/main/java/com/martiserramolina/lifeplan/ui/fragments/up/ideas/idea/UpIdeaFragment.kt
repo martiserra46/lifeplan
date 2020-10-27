f0a19c355304b27312fb6bbbc0578b610a4887cc
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:57:17 +0100

    Create UpIdeaFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/ideas/idea/UpIdeaFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.up.ideas.idea
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.up.ideas.UpIdeasFragment
+
+abstract class UpIdeaFragment<T : ViewBinding> : UpIdeasFragment<T>()
