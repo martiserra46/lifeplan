f68ee0c0ea04fb12e6410668d8f0014cec874665
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:52:10 +0100

    Create UpSituationFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/situation/UpSituationFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.up.situation
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.up.UpFragment
+
+abstract class UpSituationFragment<T : ViewBinding> : UpFragment<T>()
