1f6733d811432cd6969bdc0d527b65bfc3be5002
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:46:10 +0100

    Create NavFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/NavFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.nav
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+
+abstract class NavFragment<T : ViewBinding> : BaseFragment<T>()
