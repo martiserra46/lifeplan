5d2bb286102e3ec6f6c568fbe746629f0a1d8bf0
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:50:15 +0100

    Create UpLifeFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/life/UpLifeFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.up.life
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.up.UpFragment
+
+abstract class UpLifeFragment<T : ViewBinding> : UpFragment<T>()
