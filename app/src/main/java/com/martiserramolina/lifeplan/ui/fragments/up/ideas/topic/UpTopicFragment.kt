dee131a7fd583faf20ec52e1c9427d91d2c90b62
martiserra46 <martiserra385@gmail.com>
2020-10-27 13:56:40 +0100

    Create UpTopicFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/ideas/topic/UpTopicFragment.kt
@@ -0,0 +1,6 @@
+package com.martiserramolina.lifeplan.ui.fragments.up.ideas.topic
+
+import androidx.viewbinding.ViewBinding
+import com.martiserramolina.lifeplan.ui.fragments.up.ideas.UpIdeasFragment
+
+abstract class UpTopicFragment<T : ViewBinding> : UpIdeasFragment<T>()
