6d07c124448bf03c365cab8b659b1fbc3f88c117
martiserra46 <martiserra385@gmail.com>
2020-10-28 11:57:30 +0100

    Setup NavLifeFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/life/NavLifeFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/nav/life/NavLifeFragment.kt
@@ -3,35 +3,30 @@ package com.martiserramolina.lifeplan.ui.fragments.nav.life
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
 import com.martiserramolina.lifeplan.databinding.FragmentNavLifeBinding
-import com.martiserramolina.lifeplan.ui.activities.MainActivity
 import com.martiserramolina.lifeplan.ui.fragments.MainFragmentDirections
-import com.martiserramolina.lifeplan.ui.fragments.interfaces.OnEditMenuItemClickListener
 import com.martiserramolina.lifeplan.ui.fragments.nav.NavFragment
+import com.martiserramolina.lifeplan.viewmodels.factory.ViewModelFactory
 import com.martiserramolina.lifeplan.viewmodels.viewmodels.life.info.InfoLifeViewModel
 
-class NavLifeFragment : NavFragment<FragmentNavLifeBinding>(), OnEditMenuItemClickListener {
+class NavLifeFragment : NavFragment<FragmentNavLifeBinding>() {
 
-    private val mainActivity by lazy { activity as MainActivity }
-
-    private val viewModel by lazy {
-        ViewModelProvider(
-            this, InfoLifeViewModel.Factory(requireActivity().application)
-        ).get(InfoLifeViewModel::class.java)
-    }
+    private val viewModel by ViewModelFactory.Delegate(
+        this, InfoLifeViewModel::class.java
+    ) { InfoLifeViewModel(mainActivity.application) }
 
     override fun buildBinding(
-        inflater: LayoutInflater, container: ViewGroup?
-    ): FragmentNavLifeBinding {
-        return FragmentNavLifeBinding.inflate(inflater, container, false)
-    }
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentNavLifeBinding = FragmentNavLifeBinding.inflate(
+        inflater, container, false
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
-        setupTextTv()
+        setupTextTextView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -40,21 +35,28 @@ class NavLifeFragment : NavFragment<FragmentNavLifeBinding>(), OnEditMenuItemCli
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.life_edit_mi -> onEditMenuItemClicked()
+            R.id.life_edit_mi -> onEditMenuItemSelected()
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun setupTextTv() {
+    private fun setupTextTextView() {
         viewModel.life.observe(viewLifecycleOwner) { life ->
             binding.fragmentNavLifeDescriptionTv.text = life.lifeText
         }
     }
 
-    override fun onEditMenuItemClicked(): Boolean {
-        if (isDataLoaded()) navigateToEditLifeFragment()
-        else showWaitUntilDataIsLoadedMessage()
+    private fun onEditMenuItemSelected(): Boolean {
+        navigateToEditLifeFragmentIfDataIsLoaded()
         return true
+    }
+
+    private fun navigateToEditLifeFragmentIfDataIsLoaded() {
+        if (isDataLoaded()) {
+            navigateToEditLifeFragment()
+        } else {
+            showWaitUntilDataIsLoadedMessage()
+        }
     }
 
     private fun isDataLoaded(): Boolean = viewModel.isLifeLoaded.value == true
@@ -68,7 +70,7 @@ class NavLifeFragment : NavFragment<FragmentNavLifeBinding>(), OnEditMenuItemCli
     private fun showWaitUntilDataIsLoadedMessage() {
         Toast.makeText(
             context,
-            "Wait until all the data is loaded",
+            getString(R.string.wait_until_all_life_data_is_loaded),
             Toast.LENGTH_SHORT
         ).show()
     }
