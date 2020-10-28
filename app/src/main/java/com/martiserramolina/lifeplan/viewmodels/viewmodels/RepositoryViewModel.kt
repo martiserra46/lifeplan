442f3c227cdd328e87984c1f08411cddad3a8464
martiserra46 <martiserra385@gmail.com>
2020-10-28 10:13:01 +0100

    Remove unneeded imports in RepositoryViewModel

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/RepositoryViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/RepositoryViewModel.kt
@@ -2,11 +2,7 @@ package com.martiserramolina.lifeplan.viewmodels.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.martiserramolina.lifeplan.repository.IdeasRepository
-import com.martiserramolina.lifeplan.repository.LifeRepository
 import com.martiserramolina.lifeplan.repository.Repository
-import com.martiserramolina.lifeplan.repository.SituationRepository
-import com.martiserramolina.lifeplan.repository.room.AppDb
 
 abstract class RepositoryViewModel<T : Repository>(
     application: Application
