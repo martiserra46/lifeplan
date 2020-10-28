ee34cec7e4d85be0bbbed81d446fbe30105d5d61
martiserra46 <martiserra385@gmail.com>
2020-10-28 10:29:35 +0100

    Add Delegate class in ViewModelFactory

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/factory/ViewModelFactory.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/factory/ViewModelFactory.kt
@@ -2,16 +2,33 @@ package com.martiserramolina.lifeplan.viewmodels.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import kotlin.reflect.KProperty
 
 class ViewModelFactory<T : ViewModel>(
     private val className: Class<T>,
     private val buildInstance: () -> T
 ) : ViewModelProvider.Factory {
+
     override fun <U : ViewModel?> create(modelClass: Class<U>): U {
         if (modelClass.isAssignableFrom(className)) {
             @Suppress("UNCHECKED_CAST")
             return buildInstance() as U
         }
         throw IllegalArgumentException("Invalid ViewModel")
+    }
+
+    class Delegate<T : ViewModel>(
+        viewModelStoreOwner: ViewModelStoreOwner,
+        className: Class<T>,
+        buildInstance: () -> T
+    ) {
+        private val viewModel by lazy {
+            ViewModelProvider(
+                viewModelStoreOwner,
+                ViewModelFactory(className) { buildInstance() }
+            ).get(className)
+        }
+        operator fun getValue(thisRef: Any?, property: KProperty<*>): T = viewModel
     }
 }
