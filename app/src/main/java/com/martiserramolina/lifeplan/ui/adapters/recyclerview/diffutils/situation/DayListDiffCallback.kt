6d4022581dca16dd3354433e9662e33009ee0a64
martiserra46 <martiserra385@gmail.com>
2020-10-29 13:57:11 +0100

    Setup DayListDiffCallback

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/diffutils/situation/DayListDiffCallback.kt
@@ -0,0 +1,11 @@
+package com.martiserramolina.lifeplan.ui.adapters.recyclerview.diffutils.situation
+
+import com.martiserramolina.lifeplan.repository.room.Day
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.diffutils.ItemListDiffCallback
+
+class DayListDiffCallback(
+    oldList: List<Day>,
+    newList: List<Day>
+) : ItemListDiffCallback<Day>(oldList, newList) {
+    override fun getItemId(item: Day): Long = item.dayId
+}
