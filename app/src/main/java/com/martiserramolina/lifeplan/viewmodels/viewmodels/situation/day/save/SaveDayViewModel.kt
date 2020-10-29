d7c7a85dae08a6d3f68c94bc14892760deb33c87
martiserra46 <martiserra385@gmail.com>
2020-10-29 08:49:20 +0100

    Setup save day viewmodels

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/situation/day/save/SaveDayViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/situation/day/save/SaveDayViewModel.kt
@@ -4,23 +4,20 @@ import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.martiserramolina.lifeplan.repository.room.Day
-import com.martiserramolina.lifeplan.viewmodels.enums.SaveOperation
 import com.martiserramolina.lifeplan.viewmodels.viewmodels.situation.day.DayViewModel
 import kotlinx.coroutines.launch
 
 abstract class SaveDayViewModel(
     day: Day,
-    application: Application,
-    private val saveOperation: SaveOperation
+    application: Application
 ) : DayViewModel(day, application) {
     val daySaved = MutableLiveData<Boolean>().apply { value = false }
     fun saveDay() {
         viewModelScope.launch {
-            when (saveOperation) {
-                SaveOperation.ADD -> repository.insertDay(day)
-                SaveOperation.EDIT -> repository.updateDay(day)
-            }
+            saveDayToDatabase()
             daySaved.value = true
         }
     }
+
+    abstract suspend fun saveDayToDatabase()
 }
--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/situation/day/save/edit/EditDayViewModel.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/viewmodels/situation/day/save/edit/EditDayViewModel.kt
@@ -1,14 +1,14 @@
 package com.martiserramolina.lifeplan.viewmodels.viewmodels.situation.day.save.edit
 
 import android.app.Application
-import androidx.lifecycle.*
 import com.martiserramolina.lifeplan.repository.room.Day
-import com.martiserramolina.lifeplan.viewmodels.enums.SaveOperation
-import com.martiserramolina.lifeplan.viewmodels.viewmodels.situation.day.DayViewModel
 import com.martiserramolina.lifeplan.viewmodels.viewmodels.situation.day.save.SaveDayViewModel
-import kotlinx.coroutines.launch
 
 class EditDayViewModel(
     day: Day,
     application: Application
-) : SaveDayViewModel(day, application, SaveOperation.EDIT)
+) : SaveDayViewModel(day, application) {
+    override suspend fun saveDayToDatabase() {
+        repository.updateDay(day)
+    }
+}
