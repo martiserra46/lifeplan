3ee44daf49951958db6c23ad9d4e05495d332e04
martiserra46 <martiserra385@gmail.com>
2020-10-29 10:43:35 +0100

    Setup UpAddIdeaFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/ideas/idea/save/add/UpAddIdeaFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/up/ideas/idea/save/add/UpAddIdeaFragment.kt
@@ -1,108 +1,26 @@
 package com.martiserramolina.lifeplan.ui.fragments.up.ideas.idea.save.add
 
-import android.os.Bundle
-import android.view.*
-import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.ViewModelProvider
 import com.martiserramolina.lifeplan.R
-import com.martiserramolina.lifeplan.databinding.FragmentNavIdeasIdeaSaveBinding
-import com.martiserramolina.lifeplan.repository.enums.IdeaImportance
-import com.martiserramolina.lifeplan.ui.adapters.IdeaImportanceAdapter
 import com.martiserramolina.lifeplan.ui.fragments.up.ideas.idea.save.UpSaveIdeaFragment
+import com.martiserramolina.lifeplan.viewmodels.factory.ViewModelFactory
 import com.martiserramolina.lifeplan.viewmodels.viewmodels.ideas.idea.save.add.AddIdeaViewModel
-import java.util.*
 
 class UpAddIdeaFragment : UpSaveIdeaFragment() {
 
-    private val viewModel by lazy {
-        ViewModelProvider(
-            this,
-            AddIdeaViewModel.Factory(
-                AddIdeaFragmentArgs.fromBundle(requireArguments()).topic,
-                requireActivity().application)
-        ).get(AddIdeaViewModel::class.java)
+    override val viewModel by ViewModelFactory.Delegate(
+        this, AddIdeaViewModel::class.java
+    ) {
+        val args = AddIdeaFragmentArgs.fromBundle(requireArguments())
+        AddIdeaViewModel(args.topic, mainActivity.application)
     }
 
-    override fun buildBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentNavIdeasIdeaSaveBinding {
-        return FragmentNavIdeasIdeaSaveBinding.inflate(inflater, container, false)
-    }
+    override fun getMenuResource(): Int = R.menu.ideas_idea_add_menu
 
-    override fun getToolbar(): Toolbar = binding.fragmentNavIdeasIdeaSaveTb
-
-    override fun getToolbarTitle(): String = getString(R.string.add_idea)
+    override fun getSaveMenuItemId(): Int = R.id.ideas_idea_add_save_mi
 
     override fun navigateToPreviousFragment() {
         mainActivity.navController.navigate(
             AddIdeaFragmentDirections.actionAddIdeaFragmentToTopicFragment(viewModel.topic)
         )
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupImportanceSp()
-        navigateToPreviousFragmentAfterDbOp()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.ideas_idea_add_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.ideas_idea_add_save_mi -> {
-                saveIdea()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun setupImportanceSp() {
-        binding.fragmentNavIdeasIdeaSaveImportanceSp.apply {
-            adapter = IdeaImportanceAdapter(
-                requireContext(), R.layout.spinner_item
-            ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
-        }
-    }
-
-    private fun saveIdea() {
-        viewModel.idea.apply {
-            ideaTopicId = getTopicId()
-            ideaTitle = getTitle()
-            ideaImportance = getImportance()
-            ideaDescription = getDescription()
-            ideaLastTimeModified = getDate()
-        }
-        viewModel.addIdea()
-    }
-
-    private fun getTopicId(): Long {
-        return viewModel.topic.topicId
-    }
-
-    private fun getTitle(): String {
-        return binding.fragmentNavIdeasIdeaSaveTitleEt.text.toString()
-    }
-
-    private fun getImportance(): IdeaImportance {
-        return binding.fragmentNavIdeasIdeaSaveImportanceSp.selectedItem
-            .run { this as IdeaImportance }
-    }
-
-    private fun getDescription(): String {
-        return binding.fragmentNavIdeasIdeaSaveDescriptionEt.text.toString()
-    }
-
-    private fun getDate(): Date {
-        return Date()
-    }
-
-    private fun navigateToPreviousFragmentAfterDbOp() {
-        viewModel.ideaAdded.observe(viewLifecycleOwner) { ideaInserted ->
-            if (ideaInserted) navigateToPreviousFragment()
-        }
     }
 }
