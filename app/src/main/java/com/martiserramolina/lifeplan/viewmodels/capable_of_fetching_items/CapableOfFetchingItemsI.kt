98d7314ebdc5d8f1f39164210d435e0cb80b559f
martiserra46 <martiserra385@gmail.com>
2020-10-30 12:41:48 +0100

    Modify NUM_ITEMS_TO_FETCH to 30

--- app/src/main/java/com/martiserramolina/lifeplan/viewmodels/capable_of_fetching_items/CapableOfFetchingItemsI.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/capable_of_fetching_items/CapableOfFetchingItemsI.kt
@@ -3,7 +3,7 @@ package com.martiserramolina.lifeplan.viewmodels.capable_of_fetching_items
 import androidx.lifecycle.MutableLiveData
 
 interface CapableOfFetchingItemsI<T> {
-    companion object { private const val NUM_ITEMS_TO_FETCH = 10 }
+    companion object { private const val NUM_ITEMS_TO_FETCH = 30 }
     val itemsFetched: MutableLiveData<MutableList<T>>
     fun fetchItemsIfNotFetched(position: Long, numItems: Int = NUM_ITEMS_TO_FETCH): Boolean
 }
