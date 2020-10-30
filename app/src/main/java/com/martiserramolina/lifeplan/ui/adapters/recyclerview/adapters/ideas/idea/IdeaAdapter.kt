13b770493eb1d2fc91641cfae98e80f77b563a22
martiserra46 <martiserra385@gmail.com>
2020-10-30 10:25:38 +0100

    Setup IdeaAdapter

--- app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/ideas/idea/IdeaAdapter.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/ideas/idea/IdeaAdapter.kt
@@ -1,69 +1,22 @@
 package com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.ideas.idea
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.martiserramolina.lifeplan.databinding.RviIdeasIdeaBinding
-import com.martiserramolina.lifeplan.extensions.formatted
 import com.martiserramolina.lifeplan.repository.room.Idea
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.ItemAdapter
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.diffutils.ideas.idea.IdeaListDiffCallback
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.viewholders.ideas.idea.IdeaViewHolder
 
 class IdeaAdapter(
-    private val onIdeaClick: (Idea) -> Unit
-) : RecyclerView.Adapter<IdeaAdapter.ViewHolder>() {
+    onItemClick: (Idea) -> Unit
+) : ItemAdapter<IdeaViewHolder, IdeaListDiffCallback, Idea>(onItemClick) {
 
-    var listIdeas = emptyList<Idea>()
-        set(value) {
-            val oldValue = field
-            field = value.toList()
-            DiffUtil.calculateDiff(IdeaListDiffCallback(oldValue, field))
-                .dispatchUpdatesTo(this)
-        }
+    override fun buildViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): IdeaViewHolder = IdeaViewHolder(parent, onItemClick)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.create(parent, onIdeaClick)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(listIdeas[position])
-    }
-
-    override fun getItemCount(): Int {
-        return listIdeas.size
-    }
-
-    class ViewHolder(
-        private val binding: RviIdeasIdeaBinding,
-        private val onIdeaClick: (Idea) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-        companion object {
-            fun create(parent: ViewGroup, onIdeaClick: (Idea) -> Unit): ViewHolder {
-                return ViewHolder(
-                    RviIdeasIdeaBinding.inflate(
-                        LayoutInflater.from(parent.context), parent, false
-                    ), onIdeaClick
-                )
-            }
-        }
-
-        fun bind(idea: Idea) {
-            binding.apply {
-                rviIdeasIdeaDateTv.text = idea.ideaLastTimeModified.formatted()
-                rviIdeasIdeaTitleTv.text = idea.ideaTitle
-                rviIdeasIdeaImportanceV.setBackgroundResource(idea.ideaImportance.drawableId)
-                root.setOnClickListener { onIdeaClick(idea) }
-            }
-        }
-    }
-
-    class IdeaListDiffCallback(
-        private val oldIdeaList: List<Idea>, private val newIdeaList: List<Idea>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize() = oldIdeaList.size
-        override fun getNewListSize() = newIdeaList.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldIdeaList[oldItemPosition].ideaId == newIdeaList[newItemPosition].ideaId
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldIdeaList[oldItemPosition] == newIdeaList[newItemPosition]
-    }
+    override fun buildItemListDiffCallback(
+        oldList: List<Idea>,
+        newList: List<Idea>
+    ): IdeaListDiffCallback = IdeaListDiffCallback(oldList, newList)
 }
