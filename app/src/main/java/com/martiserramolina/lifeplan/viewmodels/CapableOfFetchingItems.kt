855964b4055d3362efcffcda12a11f705f3760e4
martiserra46 <martiserra385@gmail.com>
2020-10-30 10:54:22 +0100

    Setup CapableOfFetchingItems

---
+++ app/src/main/java/com/martiserramolina/lifeplan/viewmodels/CapableOfFetchingItems.kt
@@ -0,0 +1,32 @@
+package com.martiserramolina.lifeplan.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class CapableOfFetchingItems<T>() {
+    companion object {
+        private const val NUM_ITEMS_TO_FETCH = 20
+    }
+
+    val itemsFetched = MutableLiveData<MutableList<T>>().apply { value = mutableListOf() }
+
+    protected abstract val coroutineScope: CoroutineScope
+    private var lastFetchedPosition: Int? = null
+
+    fun fetchItemsIfNotFetched(
+        position: Int,
+        numItems: Int = NUM_ITEMS_TO_FETCH
+    ): Boolean {
+        synchronized(this) {
+            if (position == lastFetchedPosition) return false
+            lastFetchedPosition = position
+            coroutineScope.launch {
+                itemsFetched.value = itemsFetched.value?.apply { addAll(getItemsFromDatabase()) }
+            }
+            return true
+        }
+    }
+
+    abstract suspend fun getItemsFromDatabase(): List<T>
+}
