539ad86004a370264bd43e9db611a2f9f4d59250
martiserra46 <martiserra385@gmail.com>
2020-10-30 10:28:06 +0100

    Setup TopicAdapter

--- app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/ideas/topic/TopicAdapter.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/ideas/topic/TopicAdapter.kt
@@ -1,69 +1,22 @@
 package com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.ideas.topic
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.martiserramolina.lifeplan.R
-import com.martiserramolina.lifeplan.databinding.RviIdeasTopicBinding
 import com.martiserramolina.lifeplan.repository.room.Topic
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.ItemAdapter
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.diffutils.ideas.topic.TopicListDiffCallback
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.viewholders.ideas.topic.TopicViewHolder
 
 class TopicAdapter(
-    private val onTopicClick: (Topic) -> Unit
-) : RecyclerView.Adapter<TopicAdapter.ViewHolder>() {
+    onItemClick: (Topic) -> Unit
+) : ItemAdapter<TopicViewHolder, TopicListDiffCallback, Topic>(onItemClick) {
 
-    var listTopics = emptyList<Topic>()
-        set(value) {
-            val oldValue = field
-            field = value.toList()
-            DiffUtil.calculateDiff(TopicListDiffCallback(oldValue, field))
-                .dispatchUpdatesTo(this)
-        }
+    override fun buildViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TopicViewHolder = TopicViewHolder(parent, onItemClick)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.create(parent, onTopicClick)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(listTopics[position])
-    }
-
-    override fun getItemCount(): Int {
-        return listTopics.size
-    }
-
-    class ViewHolder(
-        private val binding: RviIdeasTopicBinding,
-        private val onTopicClick: (Topic) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-        companion object {
-            fun create(parent: ViewGroup, onTopicClick: (Topic) -> Unit): ViewHolder {
-                return ViewHolder(
-                    RviIdeasTopicBinding.inflate(
-                        LayoutInflater.from(parent.context), parent, false
-                    ), onTopicClick
-                )
-            }
-        }
-
-        fun bind(topic: Topic) {
-            binding.apply {
-                rviIdeasTopicTitleTv.text = topic.topicText
-                rviIdeasTopicNumIdeasTv.text = root.context
-                    .getString(R.string.num_ideas, topic.topicNumIdeas)
-                root.setOnClickListener { onTopicClick(topic) }
-            }
-        }
-    }
-
-    class TopicListDiffCallback(
-        private val oldTopicList: List<Topic>, private val newTopicList: List<Topic>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize() = oldTopicList.size
-        override fun getNewListSize() = newTopicList.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldTopicList[oldItemPosition].topicId == newTopicList[newItemPosition].topicId
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldTopicList[oldItemPosition] == newTopicList[newItemPosition]
-    }
+    override fun buildItemListDiffCallback(
+        oldList: List<Topic>,
+        newList: List<Topic>
+    ): TopicListDiffCallback = TopicListDiffCallback(oldList, newList)
 }
