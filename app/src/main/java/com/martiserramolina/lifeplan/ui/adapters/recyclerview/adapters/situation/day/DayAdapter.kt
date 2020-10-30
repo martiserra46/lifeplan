cc176276c4d476cad727ede09f0c1fb369fc50f8
martiserra46 <martiserra385@gmail.com>
2020-10-30 10:23:00 +0100

    Setup DayAdapter

--- app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/situation/day/DayAdapter.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/adapters/recyclerview/adapters/situation/day/DayAdapter.kt
@@ -1,69 +1,20 @@
 package com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.situation.day
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.martiserramolina.lifeplan.databinding.RviSituationDayBinding
-import com.martiserramolina.lifeplan.extensions.formatted
 import com.martiserramolina.lifeplan.repository.room.Day
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.adapters.ItemAdapter
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.diffutils.situation.day.DayListDiffCallback
+import com.martiserramolina.lifeplan.ui.adapters.recyclerview.viewholders.situation.day.DayViewHolder
 
 class DayAdapter(
-    private val onItemClick: (Day) -> Unit
-) : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
+    onItemClick: (Day) -> Unit
+) : ItemAdapter<DayViewHolder, DayListDiffCallback, Day>(onItemClick) {
 
-    var listDays = emptyList<Day>()
-        set(value) {
-            val oldValue = field
-            field = value.toList()
-            DiffUtil.calculateDiff(DayListDiffCallback(oldValue, field))
-                .dispatchUpdatesTo(this)
-        }
+    override fun buildViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): DayViewHolder = DayViewHolder(parent, onItemClick)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.create(parent, onItemClick)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(listDays[position])
-    }
-
-    override fun getItemCount(): Int {
-        return listDays.size
-    }
-
-    class ViewHolder(
-        private val binding: RviSituationDayBinding,
-        private val onItemClick: (Day) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-        companion object {
-            fun create(parent: ViewGroup, onItemClick: (Day) -> Unit): ViewHolder {
-                return ViewHolder(
-                    RviSituationDayBinding.inflate(
-                        LayoutInflater.from(parent.context), parent, false
-                    ), onItemClick
-                )
-            }
-        }
-
-        fun bind(day: Day) {
-            binding.apply {
-                rviSituationDayDateTv.text = day.dayDate.formatted()
-                rviSituationDayTextTv.text = day.dayText
-                rviSituationDayCircleV.setBackgroundResource(day.daySatisfaction.drawableId)
-                root.setOnClickListener { onItemClick(day) }
-            }
-        }
-    }
-
-    class DayListDiffCallback(
-        private val oldDayList: List<Day>, private val newDayList: List<Day>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize() = oldDayList.size
-        override fun getNewListSize() = newDayList.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldDayList[oldItemPosition].dayId == newDayList[newItemPosition].dayId
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldDayList[oldItemPosition] == newDayList[newItemPosition]
-    }
+    override fun buildItemListDiffCallback(
+        oldList: List<Day>, newList: List<Day>
+    ): DayListDiffCallback = DayListDiffCallback(oldList, newList)
 }
