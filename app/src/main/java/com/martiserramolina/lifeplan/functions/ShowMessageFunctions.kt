f9734f244d06f1d4a77d1701e4dd6478a232efdd
martiserra46 <martiserra385@gmail.com>
2020-11-02 18:16:03 +0100

    Setup font for snackbar

--- app/src/main/java/com/martiserramolina/lifeplan/functions/ShowMessageFunctions.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/functions/ShowMessageFunctions.kt
@@ -1,9 +1,11 @@
 package com.martiserramolina.lifeplan.functions
 
 import android.app.Activity
+import android.graphics.Typeface
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.martiserramolina.lifeplan.R
@@ -28,5 +30,12 @@ fun showMessage(view: View, messageId: Int, duration: Int = Snackbar.LENGTH_SHOR
         setTextColor(ContextCompat.getColor(context, R.color.colorSnackBarText))
         setActionTextColor(ContextCompat.getColor(context, R.color.colorSnackBarActionText))
         setAction(R.string.close) { dismiss() }
+    }.apply {
+        getView().findViewById<TextView>(
+            com.google.android.material.R.id.snackbar_text
+        ).typeface = Typeface.createFromAsset(context.assets, "fonts/open_sans.ttf")
+        getView().findViewById<TextView>(
+            com.google.android.material.R.id.snackbar_action
+        ).typeface = Typeface.createFromAsset(context.assets, "fonts/open_sans.ttf")
     }.show()
 }
