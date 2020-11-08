40164c5920f9368ac3f9f7c43b6303c4653a9e8c
martiserra46 <martiserra385@gmail.com>
2020-11-08 09:51:12 +0100

    Setup back button functionality in MainInstructionsFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/instructions/MainInstructionsFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/instructions/MainInstructionsFragment.kt
@@ -1,7 +1,10 @@
 package com.martiserramolina.lifeplan.ui.fragments.instructions
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import com.martiserramolina.lifeplan.databinding.FragmentInstructionsMainBinding
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
 
@@ -12,4 +15,18 @@ class MainInstructionsFragment : BaseFragment<FragmentInstructionsMainBinding>()
     ): FragmentInstructionsMainBinding = FragmentInstructionsMainBinding.inflate(
         inflater, container, false
     )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBackButton()
+    }
+
+    private fun setupBackButton() {
+        mainActivity.onBackPressedDispatcher
+            .addCallback(mainActivity) {
+                mainActivity.navController.navigate(
+                    MainInstructionsFragmentDirections
+                        .actionMainInstructionsFragmentToMainFragment())
+            }
+    }
 }
