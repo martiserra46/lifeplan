e7c9ca0230adeee859ec0c11f584ba87d3cbde1b
martiserra46 <martiserra385@gmail.com>
2020-11-08 11:07:05 +0100

    Setup SectionsInstructionsFragment

--- app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/instructions/sections/SectionInstructionsFragment.kt
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/instructions/sections/SectionInstructionsFragment.kt
@@ -1,15 +1,39 @@
 package com.martiserramolina.lifeplan.ui.fragments.instructions.sections
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.martiserramolina.lifeplan.databinding.FragmentInstructionsSectionBinding
 import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+import com.martiserramolina.lifeplan.utils.enums.InstructionsSection
+import com.martiserramolina.lifeplan.viewmodels.factory.ViewModelFactory
+import com.martiserramolina.lifeplan.viewmodels.viewmodels.instructions.InstructionsViewModel
 
-class SectionInstructionsFragment : BaseFragment<FragmentInstructionsSectionBinding>() {
+abstract class SectionInstructionsFragment : BaseFragment<FragmentInstructionsSectionBinding>() {
+
+    private val viewModel by ViewModelFactory.Delegate(
+        this, InstructionsViewModel::class.java
+    ) { InstructionsViewModel(getInstructionsSection(), mainActivity.application) }
+
     override fun buildBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentInstructionsSectionBinding = FragmentInstructionsSectionBinding.inflate(
         inflater, container, false
     )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.apply {
+            fragmentInstructionsSectionTitleTv.text = viewModel.title
+            fragmentInstructionsSectionDescriptionTv.text = viewModel.description
+        }
+    }
+
+    abstract fun getInstructionsSection(): InstructionsSection
 }
