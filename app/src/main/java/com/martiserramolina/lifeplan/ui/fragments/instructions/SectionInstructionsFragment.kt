defcd8e532e68c78b830d2a845bcb40d000882de
martiserra46 <martiserra385@gmail.com>
2020-11-08 10:29:53 +0100

    Create SectionInstructionsFragment

---
+++ app/src/main/java/com/martiserramolina/lifeplan/ui/fragments/instructions/SectionInstructionsFragment.kt
@@ -0,0 +1,15 @@
+package com.martiserramolina.lifeplan.ui.fragments.instructions
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.martiserramolina.lifeplan.databinding.FragmentInstructionsSectionBinding
+import com.martiserramolina.lifeplan.ui.fragments.BaseFragment
+
+class SectionInstructionsFragment : BaseFragment<FragmentInstructionsSectionBinding>() {
+    override fun buildBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentInstructionsSectionBinding = FragmentInstructionsSectionBinding.inflate(
+        inflater, container, false
+    )
+}
